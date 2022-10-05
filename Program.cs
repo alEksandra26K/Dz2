b31dc8ed2c2eef3cbbce00a11daa74f17fab338f
Александра <belka_strelka_96@mail.ru>
2022-10-05 18:37:23 +0300

    решила задачи

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿/*Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine()!);
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}*/
+
+/*Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число");
+int c = int.Parse(Console.ReadLine()!);
+if (a > c)
+{
+    if (a > b)
+    {
+       Console.WriteLine(a); 
+    }
+    else
+    {
+        Console.WriteLine(b);
+    }
+}
+else
+{
+    if (c > b)
+    {
+        Console.WriteLine(c);
+    }
+    else
+    {
+        Console.WriteLine(b);
+    }
+}*/
+
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+/*Console.WriteLine("Введите любое число");
+double a  = int.Parse(Console.ReadLine()!);
+int b = 2;
+Console.WriteLine((double)a / b);
+if ( a % 2 == 0 )
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("NO");
+}*/
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+/*Console.WriteLine("Введите любое число");
+int a = int.Parse(Console.ReadLine()!);
+int i = 1;
+while (i <= a)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+}*/
+
